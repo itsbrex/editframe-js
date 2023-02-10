b77844020a92ca7a023166aa4e076b3b68143fe7
Bryce Jackson <bryce@brycedev.com>
2023-02-09 23:29:36 -0500

    remove need to send over client id

--- src/index.ts
+++ src/index.ts
@@ -7,31 +7,24 @@ export class Editframe {
   public applications: Applications
   public videos: Videos
   private _api: Api
-  private _clientId: string
   private _host: string
   private _token: string
   private _version: number
 
-  constructor({ clientId, develop = false, host = defaultHost, token, version = 2 }: EditframeOptions) {
+  constructor({ develop = false, host = defaultHost, token, version = 2 }: EditframeOptions) {
     this._api = new Api({
-      clientId,
       fetch: initializeFetchUtil(baseUrl(host, version)),
       host,
       token,
       version,
     })
-    this._clientId = clientId
     this._host = host
     this._token = token
     this._version = version
     this.applications = new Applications(this._api)
     this.videos = new Videos({ api: this._api, develop, host: this._host })
   }
-
-  public get clientId(): string {
-    return this._clientId
-  }
-
+  
   public get host(): string {
     return this._host
   }
